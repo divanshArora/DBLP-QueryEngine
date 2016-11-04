0e73ae2dcef7abc28bb254d4fcc1ca7088d179fc
divansh <divanshar@gmail.com>
2016-11-05 01:26:08 +0530

    Added basic GUI windows

---
+++ APproject/src/DisplayWindow.java
@@ -0,0 +1,45 @@
+import java.awt.FlowLayout;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class DisplayWindow extends JFrame{
+	
+	private static final long serialVersionUID = 1L; //To make serealizable
+	private JPanel leftPanel,rightPanel;
+	
+	public DisplayWindow(String title) {
+		setTitle(title);
+		setLocationByPlatform(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		initializeAllPanels();
+		add(leftPanel);
+		add(rightPanel);
+		
+		pack();
+
+	}
+	public DisplayWindow() {
+		super("");
+	}
+	
+	private void initializeAllPanels() {
+		initializeLeftPanel();
+		initializeRightPanel();
+	}
+	
+	private void initializeLeftPanel()
+	{
+		leftPanel = new JPanel();
+	}
+	private void initializeRightPanel()
+	{
+		rightPanel = new JPanel();
+	}
+	
+
+}
--- APproject/src/mainClass.java
+++ APproject/src/mainClass.java
@@ -1,7 +1,16 @@
+import java.awt.EventQueue;
+
 public class mainClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				DisplayWindow sampleDW = new DisplayWindow("DBLP QUERY ENGINE");
+				sampleDW.setVisible(true);
+			}
+		});
+		
 	}
 
 }
