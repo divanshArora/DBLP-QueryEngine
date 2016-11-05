9eafd750edaf4750d0a65ea9d7ff9641c9ad96de
Shyam <shyam15099@iiitd.ac.in>
2016-11-05 15:51:49 +0530

    Added size

--- APproject/src/mainClass.java
+++ APproject/src/mainClass.java
@@ -7,7 +7,9 @@ public class mainClass {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				DisplayWindow sampleDW = new DisplayWindow("DBLP QUERY ENGINE");
+				sampleDW.setBounds(300, 100, 800, 600);
 				sampleDW.setVisible(true);
+				
 			}
 		});
 		//Just a comment
