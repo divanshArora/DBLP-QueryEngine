cc6610c3ae2e59c48d0ba9281f0d003899d76e6c
divansh <divanshar@gmail.com>
2016-11-28 14:57:20 +0530

    check

--- APproject/src/ComboBox.java
+++ APproject/src/ComboBox.java
@@ -5,12 +5,12 @@ import javax.swing.JPanel;
 
 public class ComboBox implements ActionListener{
 	Query gotQuery;
+	//Added a comment to test git
 	JPanel leftPanel;
 	public ComboBox(Query gotQuery, JPanel leftPanel) {
 		this.gotQuery = gotQuery;
 		this.leftPanel = leftPanel;
 	}
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
