388526e36b8c95f675d24a438dd5a11ec40107a2
divansh <divanshar@gmail.com>
2016-11-28 12:04:25 +0530

    idk still

---
+++ APproject/src/MainClass.java
@@ -0,0 +1,19 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+
+				DisplayWindow DW = new DisplayWindow("DBLP QUERY ENGINE");
+				DW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				DW.setVisible(true);
+			}
+		});
+		
+	}
+}
